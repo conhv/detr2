f455e485e8f09e9e485b6bfb638e6b6e8a709b0d
conhv <hovancon1998@gmail.com>
2022-04-24 14:48:23 +0700

    create efficientNet backbone

--- models/backbone.py
+++ models/backbone.py
@@ -80,6 +80,30 @@ class BackboneBase(nn.Module):
         return out
 
 
+class BackboneEfficientNetBase(nn.Module):
+
+    def __init__(self, backbone: nn.Module, train_backbone: bool, num_channels: int, return_interm_layers: bool):
+        super().__init__()
+        for name, parameter in backbone.named_parameters():
+            if not train_backbone:
+                parameter.requires_grad_(False)
+        if return_interm_layers:
+            return_layers = {'features': "0"}
+        else:
+            return_layers = {'features': "0"}
+        self.body = IntermediateLayerGetter(backbone, return_layers=return_layers)
+        self.num_channels = num_channels
+
+    def forward(self, tensor_list: NestedTensor):
+        xs = self.body(tensor_list.tensors)
+        out: Dict[str, NestedTensor] = {}
+        for name, x in xs.items():
+            m = tensor_list.mask
+            assert m is not None
+            mask = F.interpolate(m[None].float(), size=x.shape[-2:]).to(torch.bool)[0]
+            out[name] = NestedTensor(x, mask)
+        return out
+
 class Backbone(BackboneBase):
     """ResNet backbone with frozen BatchNorm."""
     def __init__(self, name: str,
@@ -90,6 +114,19 @@ class Backbone(BackboneBase):
             replace_stride_with_dilation=[False, False, dilation],
             pretrained=is_main_process(), norm_layer=FrozenBatchNorm2d)
         num_channels = 512 if name in ('resnet18', 'resnet34') else 2048
+        super().__init__(backbone, train_backbone, num_channels, return_interm_layers)
+
+class BackboneEfficientNet(BackboneEfficientNetBase):
+    """ResNet backbone with frozen BatchNorm."""
+    def __init__(self, name: str,
+                 train_backbone: bool,
+                 return_interm_layers: bool,
+                 dilation: bool):
+
+        backbone = getattr(torchvision.models, name)(
+            replace_stride_with_dilation=[False, False, dilation],
+            pretrained='True', norm_layer=FrozenBatchNorm2d)
+        num_channels = 1280 if name in ('efficientnet_b1') else 1280
         super().__init__(backbone, train_backbone, num_channels, return_interm_layers)
 
 
